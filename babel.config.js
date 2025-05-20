abefbff4d859e7f14c7412eb52b71b0be20d41c2
Domen Persolja <bekaxp.f1@gmail.com>
2025-05-20 17:26:35 +0200

    configs

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,17 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    'babel-plugin-react-compiler',
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: ['.js', '.ts', '.tsx', '.json'],
+        alias: {
+          '@': './src',
+        },
+      },
+    ],
+    'react-native-reanimated/plugin',
+  ],
 };
